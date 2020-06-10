b2d8aca688ca0e28b76a3c79c9fcfdcc51fed968
unknown <dohuycuong92@gmail.com>
2020-06-09 22:34:17 -0700

    heroku setup

--- public/js/test.js
+++ public/js/test.js
@@ -1,12 +1,3 @@
-console.log('testing')
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then ((data) => {
-//         console.log(data)
-//     })
-// })
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#messageOne')
@@ -19,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
         messageTwo.textContent = ""
         return messageOne.textContent = "You must provide address"
     } 
-    fetch('http://localhost:3000/weather?address='+location).then ((r) => {
+    fetch('/weather?address='+location).then ((r) => {
     r.json().then((data) => {
         if (data.error) {
             messageTwo.textContent = ""
